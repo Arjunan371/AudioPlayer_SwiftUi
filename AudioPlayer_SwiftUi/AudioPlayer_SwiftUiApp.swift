4dc32a167a3545f77e88f4a51622bb7363b8f2e6
Arjunan371 <m.arjunan371@gmail.com>
2024-03-26 10:07:12 +0530

    Initial Commit

---
+++ AudioPlayer_SwiftUi/AudioPlayer_SwiftUiApp.swift
@@ -0,0 +1,17 @@
+//
+//  AudioPlayer_SwiftUiApp.swift
+//  AudioPlayer_SwiftUi
+//
+//  Created by Arjunan on 26/03/24.
+//
+
+import SwiftUI
+
+@main
+struct AudioPlayer_SwiftUiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
